2860373a3f446cb9cf760d2d01c85ec94038bb63
itrunc <mophee@163.com>
2014-12-08 16:50:43 +0800

    add /register

--- cloud/app.js
+++ cloud/app.js
@@ -8,18 +8,48 @@ app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
-app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
+//app.get('/hello', function(req, res) {
+//  res.render('hello', { message: 'Congrats, you just set up your app!' });
+//});
+
+app.get('/index', function(req, res) {
+    res.send('Hello Index');
+});
+
+app.get('/register', function (req, res) {
+    AV.User.logOut(); //已登录用户退出
+    res.render('register');
 });
 
 app.get('/login', function(req, res) {
-    var currentUser = AV.User.current();
-    if( currentUser ) {
+    var currentUser = AV.User.current(); //获取当前用户
+    if( currentUser ) { //已登录
         res.redirect('/hello');
     }
-    else {
-        res.redirect('/');
+    else { //未登录
+        res.render('login');
     }
+});
+
+app.post('/register', function (req, res) {
+    var username = req.params.usrname;
+    var passwd = req.params.passwd;
+    var confirmPasswd = req.params.confirmpasswd;
+    if( passwd != confirmPasswd ) {
+        res.send('确认密码与密码不一致');
+        return;
+    }
+    var user = new AV.User();
+    user.set('username', username);
+    user.set('password', passwd);
+    user.signUp(null, {
+        success: function(user) {
+            res.redirect('/hello');
+        },
+        error: function(user, error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
 });
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
