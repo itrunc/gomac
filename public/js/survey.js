286affee006b2f7fa844e3c3d6202c687d51a282
itrunc <mophee@163.com>
2014-12-15 17:57:34 +0800

    update survey app

--- public/js/survey.js
+++ public/js/survey.js
@@ -51,24 +51,12 @@ $(function(){
             //初始化表单验证
             this.validator = this.$el.find('form').kendoValidator().data("kendoValidator");
 
-            this.saveSuccess = false;
+            //this.saveSuccess = false;
 
             this.$el.off('hidden.bs.modal');
             this.$el.on('hidden.bs.modal', function (e) {
                 self.undelegateEvents();
             })
-
-            //初始化提示信息
-            this.notification = $('<span></span>').kendoNotification({
-                appendTo: '.modal-msg',
-                hide: function(e) {
-                    self.$el.find('#btn-save-survey').removeClass('disabled');
-                    if( self.saveSuccess ) {
-                        self.$el.modal('hide');
-                        self.saveSuccess = false;
-                    }
-                }
-            }).data("kendoNotification");
 
             //背景拾色器
             this.$el.find('#survey-background-color').kendoColorPicker({
@@ -90,7 +78,11 @@ $(function(){
             $(e.target).addClass('disabled');
 
             if( !self.validator.validate() ) {
-                self.notification.error('您未完全按照要求填写表单，请根据提示填写');
+                BootstrapDialog.alert({
+                    title: '危险动作！',
+                    message: '<div class="alert alert-danger">您未完全按照要求填写表单，请根据提示填写</div>',
+                    type: BootstrapDialog.TYPE_DEFAULT
+                });
                 return false;
             }
 
@@ -116,81 +108,115 @@ $(function(){
                     else {//编辑时
                         self.options.surveyView.render();
                     }
-                    self.notification.success('您已经成功地保存该调查，本弹窗即将关闭');
-                    self.saveSuccess = true;
+                    BootstrapDialog.show({
+                        title: '保存成功',
+                        message: '您已经成功地保存该调查',
+                        type: BootstrapDialog.TYPE_SUCCESS,
+                        closable: false,
+                        buttons: [{
+                            label: '关闭',
+                            cssClass: 'btn-success',
+                            action: function( dialog ) {
+                                dialog.close();
+                            }
+                        }],
+                        onhidden: function(e) {
+                            self.$el.modal('hide');
+                        }
+                    });
 
                 },
                 error: function(model, error) {
-                    self.notification.error('保存失败：'+error.message);
+                    BootstrapDialog.danger( '保存失败：'+error.message );
                 }
             });
 
         }
     });
 
-    //问题表单
+    //题目定义表单
     var SurveyItemFormView = AV.View.extend({
-        el: '#survey-sub-modal',
         template: _.template( $('#survey-item-form-tpl').html() ),
-        events: {
-            'click #btn-save-survey-item': 'saveSurveyItem'
-        },
+        events: {},
         initialize: function() {
             var self = this;
-            self.$el.find('.modal-body').html( this.template(this.model.toJSON()) );
 
-            //初始化表单验证
-            this.validator = this.$el.find('form').kendoValidator().data("kendoValidator");
-
-            this.saveSuccess = false;
-
-            this.$el.off('hidden.bs.modal');
-            this.$el.on('hidden.bs.modal', function (e) {
-                self.undelegateEvents();
-            })
-
-            //初始化提示信息
-            this.notification = $('<span></span>').kendoNotification({
-                appendTo: '.modal-msg',
-                hide: function(e) {
-                    self.$el.find('#btn-save-survey-item').removeClass('disabled');
-                    if( self.saveSuccess ) {
-                        self.$el.modal('hide');
-                        self.saveSuccess = false;
+            BootstrapDialog.show({
+                title: self.options.isCreate ? '添加题目' : '编辑题目',
+                message: self.template( self.model.toJSON() ),
+                type: BootstrapDialog.TYPE_DEFAULT,
+                nl2br: false,
+                buttons: [{
+                    label: '取消',
+                    cssClass: 'btn-default',
+                    icon: 'glyphicon glyphicon-ban-circle',
+                    action: function(dialog) {
+                        dialog.close();
                     }
+                }, {
+                    label: '保存',
+                    cssClass: 'btn-success',
+                    icon: 'glyphicon glyphicon-saved',
+                    action: function(dialog) {
+                        self.saveSurveyItem(dialog);
+                    }
+                }],
+                onshown: function(dialog) {
+                    self.validator = dialog.getModalBody().find('form').kendoValidator().data("kendoValidator");
                 }
-            }).data("kendoNotification");
+            });
         },
-        saveSurveyItem: function(e) {
+        saveSurveyItem: function(dialog) {
             var self = this;
+            var thatDialog = dialog;
             var currentUser = AV.User.current();
 
-            $(e.target).addClass('disabled');
-
-            self.model.set('title', this.$el.find('#survey-item-title').val());
-            self.model.set('intro', this.$el.find('#survey-item-intro').val());
-            self.model.set('items', this.$el.find('#survey-item-items').val());
-
-            if( self.options.isCreate ) {
-                self.model.set('survey', self.options.surveyView.model);
-                self.model.set('user', currentUser);
-                self.model.set('ACL', new AV.ACL(currentUser));
+            if( !this.validator.validate() ) {
+                BootstrapDialog.alert({
+                    title: '危险动作！',
+                    message: '<div class="alert alert-danger">您未完全按照要求填写表单，请根据提示填写</div>',
+                    type: BootstrapDialog.TYPE_DEFAULT
+                });
+                return false;
             }
 
-            self.model.save(null, {
+            this.model.set('title', dialog.getModalBody().find('#survey-item-title').val());
+            this.model.set('intro', dialog.getModalBody().find('#survey-item-intro').val());
+            this.model.set('items', dialog.getModalBody().find('#survey-item-items').val());
+
+            if( this.options.isCreate ) {
+                this.model.set('survey', this.options.surveyView.model);
+                this.model.set('user', currentUser);
+                this.model.set('ACL', new AV.ACL(currentUser));
+            }
+
+            this.model.save(null, {
                 success: function(model) {
-                    self.options.surveyView.itemCount++;
-                    self.options.surveyView.render();
-                    self.notification.success('您已经成功保存，本弹窗即将关闭');
-                    self.saveSuccess = true;
+                    BootstrapDialog.show({
+                        title: '保存成功',
+                        message: '您已经成功地保存',
+                        type: BootstrapDialog.TYPE_SUCCESS,
+                        closable: false,
+                        buttons: [{
+                            label: '关闭',
+                            cssClass: 'btn-success',
+                            action: function( dialog ) {
+                                dialog.close();
+                            }
+                        }],
+                        onhidden: function(e) {
+                            thatDialog.close();
+                            self.options.surveyView.itemCount++;
+                            self.options.surveyView.render();
+                        }
+                    });
                 },
                 error: function(model, error) {
-                    self.notification.error('保存失败：'+error.message);
+                    BootstrapDialog.danger( '保存失败：'+error.message );
                 }
             });
         }
     });
-
 
     //调查视图定义
     var SurveyView = AV.View.extend({
@@ -200,7 +226,8 @@ $(function(){
         events: {
             'click .btn-edit-survey': 'editSurvey',
             'click .btn-remove-survey': 'removeSurvey',
-            'click .btn-add-survey-item': 'addSurveyItem'
+            'click .btn-add-survey-item': 'addSurveyItem',
+            'click .btn-view-survey-item': 'viewSurveyItem'
         },
         initialize: function() {
             var self = this;
@@ -244,10 +271,29 @@ $(function(){
             }
         },
         removeSurvey: function(e) { //删除调查
-            if( confirm('您将删除《'+this.model.get('title')+'》？') ) {
-                //TODO: 级联删除问题
-                this.model.destroy();
-            }
+            var self = this;
+
+            BootstrapDialog.show({
+                title: '删除确认',
+                message: '<div class="alert alert-danger"><i class="glyphicon glyphicon-warning-sign"></i> 您确定要删除《'+this.model.get('title')+'》吗？</div>',
+                type: BootstrapDialog.TYPE_DEFAULT,
+                buttons: [{
+                    label: '取消',
+                    cssClass: 'btn-success',
+                    icon: 'glyphicon glyphicon-ban-circle',
+                    action: function(dialog) {
+                        dialog.close();
+                    }
+                }, {
+                    label: '删除',
+                    cssClass: 'btn-danger',
+                    icon: 'glyphicon glyphicon-saved',
+                    action: function(dialog) {
+                        self.model.destroy();
+                        dialog.close();
+                    }
+                }]
+            });
         },
         addSurveyItem: function(e) { //添加调查问题
             var self = this;
@@ -256,7 +302,29 @@ $(function(){
                 surveyView: self,
                 isCreate: true
             });
-            $('#survey-sub-modal').modal('show');
+        },
+        viewSurveyItem: function(e) {
+            var self = this;
+            var surveyItemCollectionView = new SurveyItemCollectionView({
+                surveyView: self
+            });
+        }
+    });
+
+    //题目视图
+    var SurveyItemView = AV.View.extend({
+        tagName: 'li',
+        className: 'list-group-item',
+        template: _.template( $('#survey-item-tpl').html() ),
+        events: {},
+        initialize: function() {
+            _.bindAll(this, 'render', 'remove');
+            this.model.bind('change', this.render);
+            this.model.bind('destroy', this.remove);
+        },
+        render: function() {
+            $(this.el).html( this.template( this.model.toJSON() ) );
+            return this;
         }
     });
 
@@ -310,6 +378,57 @@ $(function(){
             else {
                 redirect('/login');
             }
+        }
+    });
+
+    //题目集合视图
+    var SurveyItemCollectionView = AV.View.extend({
+        template: _.template( $('#survey-item-collection-tpl').html() ),
+        events: {},
+        initialize: function() {
+            var self = this;
+
+            self.dialog = new BootstrapDialog({
+                title: self.options.surveyView.model.get('title'),
+                message: self.template(),
+                type: BootstrapDialog.TYPE_DEFAULT,
+                nl2br: false,
+                size: BootstrapDialog.SIZE_WIDE,
+                buttons: [{
+                    label: '关闭',
+                    cssClass: 'btn-default',
+                    icon: 'glyphicon glyphicon-ban-circle',
+                    action: function(dialog) { dialog.close(); }
+                }],
+                onshown: function(dialog) {
+                    self.surveyItemCollection = new SurveyItemCollection;
+                    self.surveyItemCollection.query = new AV.Query( SurveyItem );
+                    self.surveyItemCollection.query.equalTo('user', AV.User.current());
+                    self.surveyItemCollection.query.equalTo('survey', self.options.surveyView.model);
+                    self.surveyItemCollection.bind('add', self.addOne);
+                    self.surveyItemCollection.bind('reset', self.addAll);
+                    self.surveyItemCollection.bind('all', self.render);
+                    self.surveyItemCollection.fetch();
+                }
+            });
+
+            self.dialog.open();
+        },
+        addOne: function(surveyItem) {
+            var self = this;
+            var view = new SurveyItemView({
+                model: surveyItem,
+                collectionView: self
+            });
+            self.dialog.getModalBody().find('.list-group').append( view.render().el );
+        },
+
+        addAll: function(collection, filter) {
+            var self = this;
+            console.log(self.dialog);
+
+            self.dialog.getModalBody().find('.list-group').html('');
+            self.surveyItemCollection.each( this.addOne );
         }
     });
 
